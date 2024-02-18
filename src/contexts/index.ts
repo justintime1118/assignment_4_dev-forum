af8932a6e54fe1383420044d3715f1c566229d40
justinYoo <ugh1118@gmail.com>
2024-02-19 02:57:56 +0900

    [feat] set URIs and mapped each controller

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import authController from "./auth/auth.controller";
+import commentsController from "./comments/comments.controller";
+import forumsController from "./forums/forums.controller";
+import likesController from "./likes/likes.controller";
+import postsController from "./posts/posts.controller";
 import usersController from "./users/users.controller";
 
 export const controllers = Router();
 
 controllers.use("/auth", authController);
-controllers.use("/users", usersController);
+controllers.use("/forums", forumsController);
+controllers.use("/forums/:forumName/posts", postsController);
+controllers.use("/forums/:forumName/posts/:postId/likes", likesController);
+controllers.use(
+  "/forums/:forumName/posts/:postId/comments",
+  commentsController
+);
+controllers.use("/users/:userId/likes", usersController);
