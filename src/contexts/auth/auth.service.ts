9fe82b46322adb4e52dacb6b7fe10e1ddd6df670
justinYoo <ugh1118@gmail.com>
2024-02-19 02:09:46 +0900

    [refactor] change error handling layer

--- src/contexts/auth/auth.service.ts
+++ src/contexts/auth/auth.service.ts
@@ -12,12 +12,21 @@ class AuthService {
     const encryptedPassword = await bcrypt.hash(password, 10);
 
     let response: APIResponse;
-    const newUser = await usersRepository.insertUser(email, encryptedPassword);
-    if (!newUser) {
-      res.status(404);
-      response = createErrorResponse("given email might already be occupied");
-    } else {
-      response = createSuccessResponse(newUser);
+    try {
+      const newUser = await usersRepository.insertUser(
+        email,
+        encryptedPassword
+      );
+      if (!newUser) {
+        res.status(404);
+        response = createErrorResponse("given email might already be occupied");
+      } else {
+        response = createSuccessResponse(newUser);
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500);
+      response = createErrorResponse("Prisma error");
     }
     res.json(response);
   }
@@ -26,31 +35,37 @@ class AuthService {
     const { email, password } = req.body;
     let response: APIResponse;
     // 해당 이메일로 가입된 사용자가 있는지 확인
-    const user = await usersRepository.findUserByEmail(email);
-    if (!user) {
-      response = createErrorResponse("email doesn't exit");
-      res.status(404);
-      return res.json(response);
-    }
-
-    // 비밀번호가 제대로 입력되었는지 확인
-    const isValid = await bcrypt.compare(password, user.encryptedPassword);
-    if (!isValid) {
-      res.status(404);
-      response = createErrorResponse("password is invalid");
-      return res.json(response);
-    }
-
-    // 엑세스 토큰 발급해서 리턴
-    const accessToken = jwt.sign(
-      { email: user.email },
-      process.env.JWT_SECRET_KEY as string,
-      {
-        subject: user.email,
+    try {
+      const user = await usersRepository.findUserByEmail(email);
+      if (!user) {
+        response = createErrorResponse("email doesn't exit");
+        res.status(404);
+        return res.json(response);
       }
-    );
-    response = createSuccessResponse(accessToken);
-    res.json(response);
+
+      // 비밀번호가 제대로 입력되었는지 확인
+      const isValid = await bcrypt.compare(password, user.encryptedPassword);
+      if (!isValid) {
+        res.status(404);
+        response = createErrorResponse("password is invalid");
+        return res.json(response);
+      }
+
+      // 엑세스 토큰 발급해서 리턴
+      const accessToken = jwt.sign(
+        { email: user.email },
+        process.env.JWT_SECRET_KEY as string,
+        {
+          subject: user.email,
+        }
+      );
+      response = createSuccessResponse(accessToken);
+      res.json(response);
+    } catch (e) {
+      response = createErrorResponse("Prisma error");
+      res.status(500);
+      return res.json(response);
+    }
   }
 }
 
--- src/contexts/users/users.repository.ts
+++ src/contexts/users/users.repository.ts
@@ -2,43 +2,24 @@ import prismaCient from "../../prisma/client.prisma";
 
 class UsersRepository {
   async insertUser(email: string, encryptedPassword: string) {
-    try {
-      const newUser = await prismaCient.users.create({
-        data: { email, encryptedPassword },
-        select: { id: true, email: true, createdAt: true },
-      });
-      return newUser;
-    } catch (e) {
-      console.log(e);
-      return null;
-    }
+    const newUser = await prismaCient.users.create({
+      data: { email, encryptedPassword },
+      select: { id: true, email: true, createdAt: true },
+    });
+    return newUser;
   }
 
   async findUserByEmail(email: string) {
-    try {
-      const newUser = await prismaCient.users.findUnique({
-        where: { email: email },
-      });
-      return newUser;
-    } catch (e) {
-      console.log(e);
-      return null;
-    }
+    const newUser = await prismaCient.users.findUnique({
+      where: { email: email },
+    });
+    return newUser;
   }
 
   async findAllUsers() {
-    try {
-      const users = await prismaCient.users.findMany();
-      return users;
-    } catch (e) {
-      console.log(e);
-      return null;
-    }
+    const users = await prismaCient.users.findMany();
+    return users;
   }
-
-  async updateUser() {}
-
-  async deleteUser() {}
 }
 
 const usersRepository = new UsersRepository();
